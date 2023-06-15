ac2bed00368182749f30a1bd9a314dca2601b53d
ZezethuM <zezethu15@gmail.com>
2023-06-15 14:49:18 -0700

    C program that prints a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle, followed by a new line");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
